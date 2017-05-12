6a06cb6ac03fc8ebacb3ee606f17ddaa19921604
quick0mortal <747867858@qq.com>
2017-05-12 11:13:21 +0800

    Add files via upload

---
+++ build_cross_compiler.sh
@@ -0,0 +1,33 @@
+# /bin/bash
+
+mv ./mips-decstation.linux-xgcc.tgz /
+
+cd /
+tar -xzvf /mips-decstation.linux-xgcc.tgz 
+
+cd -
+
+cd ./NachOS-4.1a/coff2noff/
+
+./configure
+
+sed -i "s/CFLAGS= -g -O2 -DRDATA -DHAVE_CONFIG_H -I@top_dir@/CFLAGS= -g -O2 -m32 -DRDATA -DHAVE_CONFIG_H -I@top_dir@/g" ./Makefile
+
+sed -i "s/LD=gcc/LD=gcc -m32/g" ./Makefile
+
+make
+
+cd ./../code/test
+
+sed -i "s/\#CPP = \/usr\/local\/nachos\/lib\/gcc-lib\/decstation-ultrix\/2.95.2\/cpp/CPP = \/usr\/local\/nachos\/lib\/gcc-lib\/decstation-ultrix\/2.95.2\/cpp/g" ./Makefile.dep
+
+sed -i "s/CPP = \/usr\/local\/nachos\/current\/lib\/gcc-lib\/decstation-ultrix\/2.7.2.1\/cpp/\#CPP = \/usr\/local\/nachos\/current\/lib\/gcc-lib\/decstation-ultrix\/2.7.2.1\/cpp/g" ./Makefile.dep
+
+sed -i "s/\#GCCDIR = \/usr\/local\/nachos\/bin\/decstation-ultrix-/GCCDIR = \/usr\/local\/nachos\/bin\/decstation-ultrix-/g" ./Makefile.dep
+
+sed -i "s/GCCDIR = \/usr\/local\/nachos\/current\/bin\/decstation-ultrix-/\#GCCDIR = \/usr\/local\/nachos\/current\/bin\/decstation-ultrix-/g" ./Makefile.dep
+
+make
+
+
+
